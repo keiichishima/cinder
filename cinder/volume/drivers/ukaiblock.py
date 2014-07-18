8d7d035901af3d0f34684b96acec35c096e17e4a
Keiichi SHIMA <keiichi@iijlab.net>
2014-07-18 12:51:02 +0900

    import _

--- cinder/volume/drivers/ukaiblock.py
+++ cinder/volume/drivers/ukaiblock.py
@@ -33,6 +33,7 @@ from oslo.config import cfg
 from cinder.brick.remotefs import remotefs
 from cinder import exception
 from cinder.image import image_utils
+from cinder.openstack.common.gettextutils import _
 from cinder.openstack.common import log as logging
 from cinder.openstack.common import processutils as putils
 from cinder.openstack.common import units
